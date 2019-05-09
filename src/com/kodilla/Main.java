714db91287925cc2e7f8a054c0c22614a147107b
Niedamir <rafallewandowski09@gmail.com>
2019-05-09 21:03:41 +0200

    v. 0.0.1 Start project

---
+++ src/com/kodilla/Main.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public void start(Stage primaryStage){
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
